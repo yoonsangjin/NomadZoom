7edf7f9079f8e1030880360a6daa9e2d919a135b
yoonsangjin <yoonsj184@gmail.com>
2023-01-25 22:03:31 +0900

    initial commit

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import http from "http";
+// import WebSocket from "ws";
+import SocketIO from "socket.io";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const server = http.createServer(app);
+const io = SocketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("room", (msg) => console.log(msg));
+});
+
+// const wss = new WebSocket.Server({ server });
+// function onSocketClose() {
+//   console.log("Disconnected from the Browser ❌");
+// }
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anonymous";
+//   console.log("Connected to Browser ✅");
+//   socket.on("close", onSocketClose);
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
+
+server.listen(3000, handleListen);
